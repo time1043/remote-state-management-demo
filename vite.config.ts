e06fa5b86d899a5de6d62e263fa52e401a57305e
oswin902m <1665434994@qq.com>
2026-05-18 16:03:11 +0800

    init docs

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import babel from "@rolldown/plugin-babel";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
