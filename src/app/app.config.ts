a3b130851fb0477db30a83f5727f828990e569e5
oswin902m <1665434994@qq.com>
2026-05-18 15:15:51 +0800

    setup http-client (ng built-in)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners()],
+  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient(withFetch())],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
+
+type AdviceResponse = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
 
 @Component({
   selector: 'app-root',
@@ -6,6 +14,8 @@ import { Component, OnInit, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App implements OnInit {
+  private readonly http = inject(HttpClient);
+
   protected readonly advice = signal('Advice ...');
   protected readonly isLoading = signal(false);
 
@@ -13,13 +23,17 @@ export class App implements OnInit {
     this.getAdvice();
   }
 
-  async getAdvice() {
+  getAdvice() {
     this.isLoading.set(true);
 
-    const response = await fetch('https://api.adviceslip.com/advice');
-    const data = await response.json();
-
-    this.advice.set(data.slip.advice);
-    this.isLoading.set(false);
+    // this.http.get(): Observable  // rxjs
+    this.http
+      .get<AdviceResponse>('https://api.adviceslip.com/advice', {
+        timeout: 5000,
+      })
+      .subscribe((data) => {
+        this.advice.set(data.slip.advice);
+        this.isLoading.set(false);
+      });
   }
 }
