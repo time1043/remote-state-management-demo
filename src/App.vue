b77c52799cef4f9ebb673a0bd7b91dada1cac2f1
oswin902m <1665434994@qq.com>
2026-05-18 14:28:01 +0800

    init: advice-demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>app</div>
+  <main>
+    <h1>Advice App</h1>
+    <p>{{ advice }}</p>
+    <button @click="getAdvice">Get Advice</button>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const advice = ref("Advice ...");
+
+async function getAdvice() {
+  // https://api.adviceslip.com/
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  advice.value = data.slip.advice;
+}
+
+onMounted(() => {
+  getAdvice();
+});
+</script>
 
 <style scoped></style>
