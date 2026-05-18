f55aa9d126a501bb422a20cb98a48c7d8ccd69da
oswin902m <1665434994@qq.com>
2026-05-18 14:10:57 +0800

    init: manual remote state management with useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("Advice ...");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getAdvice() {
+    setIsLoading(true);
+
     // https://api.adviceslip.com/
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
 
     setAdvice(data.slip.advice);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -18,8 +22,10 @@ export default function App() {
   return (
     <main>
       <h1>Advice App</h1>
-      <p>{advice}</p>
-      <button onClick={getAdvice}>Get Advice</button>
+      <p>{isLoading ? "Loading..." : advice}</p>
+      <button onClick={getAdvice} disabled={isLoading}>
+        Get Advice
+      </button>
     </main>
   );
 }
