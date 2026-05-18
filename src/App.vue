66d871c1e78fb6861b90c1b76ac4b286f8cc993f
oswin902m <1665434994@qq.com>
2026-05-18 14:28:45 +0800

    init: manual remote state management with ref

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <h1>Advice App</h1>
-    <p>{{ advice }}</p>
-    <button @click="getAdvice">Get Advice</button>
+    <p>{{ isLoading ? "Loading..." : advice }}</p>
+    <button @click="getAdvice" :disabled="isLoading">Get Advice</button>
   </main>
 </template>
 
@@ -10,12 +10,14 @@
 import { onMounted, ref } from "vue";
 
 const advice = ref("Advice ...");
+const isLoading = ref(false);
 
 async function getAdvice() {
-  // https://api.adviceslip.com/
+  isLoading.value = true;
   const response = await fetch("https://api.adviceslip.com/advice");
   const data = await response.json();
   advice.value = data.slip.advice;
+  isLoading.value = false;
 }
 
 onMounted(() => {
