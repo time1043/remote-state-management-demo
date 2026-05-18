a04a884ef3f7777e9ded9ab4e630bed8b8da6afd
oswin902m <1665434994@qq.com>
2026-05-18 14:10:08 +0800

    init: advice-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>App</div>;
+  const [advice, setAdvice] = useState("Advice ...");
+
+  async function getAdvice() {
+    // https://api.adviceslip.com/
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <main>
+      <h1>Advice App</h1>
+      <p>{advice}</p>
+      <button onClick={getAdvice}>Get Advice</button>
+    </main>
+  );
 }
